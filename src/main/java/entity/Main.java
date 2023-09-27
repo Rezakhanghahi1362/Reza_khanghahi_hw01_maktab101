4260ea707f0195116bb5f153e7b4533257fa9d9a
Saba ICT Co <Rezakhanghahi1362@gmail.com>
2023-09-26 23:38:34 -0700

    set project

---
+++ src/main/java/entity/Main.java
@@ -0,0 +1,16 @@
+package entity;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+
+    public static void main(String[] args)throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","932305878");
+    }
+
+
+}
