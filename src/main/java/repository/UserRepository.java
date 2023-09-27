0b6efa472cb47b79eb016f88b1a7ee5d33579b44
Saba ICT Co <Rezakhanghahi1362@gmail.com>
2023-09-27 01:57:05 -0700

    creat CategoryRepository

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,8 +1,29 @@
 package repository;
 
+import connection.JdbcConnection;
+import entity.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserRepository {
 
+    public Connection connection= JdbcConnection.getConnection();
 
+    public UserRepository() throws SQLException {
+    }
 
+    public int save(User user) throws SQLException {
+
+        String query = "INSERT IN TO USERS(name, username, email, password) values(?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1,user.getName());
+        preparedStatement.setString(2,user.getUserName());
+        preparedStatement.setString(3,user.getEmail());
+        preparedStatement.setString(4,user.getPassword());
+        int result=preparedStatement.executeUpdate();
+        return result;
+    }
 
 }
