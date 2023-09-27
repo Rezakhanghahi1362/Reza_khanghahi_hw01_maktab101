4e638f980c04d6f9db1bb866b886de9d1450526c
Saba ICT Co <Rezakhanghahi1362@gmail.com>
2023-09-27 00:10:56 -0700

    creat users Table

--- src/main/java/entity/sql/table.sql
+++ src/main/java/entity/sql/table.sql
@@ -1,1 +1,12 @@
 
+CREATE table if not EXISTS USERS(
+
+    id serial primary key ,
+    name varchar(50) not null ,
+    username varchar(50) not null ,
+    email varchar(50) not null ,
+    password varchar(50) not null
+
+
+
+)
