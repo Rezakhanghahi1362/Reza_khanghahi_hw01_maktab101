d19e852f2e21ec3211804443261801a5054007ec
Saba ICT Co <Rezakhanghahi1362@gmail.com>
2023-10-19 08:04:28 -0700

    add validation class

--- src/main/java/repository/SherHolderBrand.java
+++ src/main/java/repository/SherHolderBrand.java
@@ -1,5 +1,39 @@
 package repository;
 
+import connection.JdbcConnection;
+
+import java.sql.*;
+
 public class SherHolderBrand {
 
+    private static Connection connection;
+
+    static {
+        try {
+            connection = JdbcConnection.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public SherHolderBrand() throws SQLException{
+        
+    }
+    public static int save(SherHolderBrand sherHolderBrand) throws SQLException{
+
+        String query = "INSERT IN TO sherholderbrand(sherholderid,brandid) valus(?,?,)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        Object shareholderbrand = null;
+        preparedStatement.setString(1, String.valueOf(shareholderbrand.getClass()));
+        preparedStatement.setString(2, String.valueOf(shareholderbrand.getClass()));
+        
+        int result = preparedStatement.executeUpdate();
+        return result ;
+        
+    }
+
 }
+    
+    
+        
